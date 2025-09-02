5bb9e47da314226e72de544e0756a49fff4946db
Valery Semenenko <gearmobile@gmail.com>
2025-09-02 21:23:07 +0300

    continue

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,9 @@
+import { LowerCasePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'tl-root',
-  imports: [],
+  imports: [UpperCasePipe, LowerCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
@@ -11,8 +12,24 @@ export class App {
   readonly group = 'guest';
 
   greeting(strings: TemplateStringsArray, user: string, group: string): string {
-    console.log(strings);
+    console.log('strings ::', strings);
 
     return `${user}@${group}:/${group}/${user}$`;
+  }
+
+  rest(strings: TemplateStringsArray, ...placeholders: string[]): void {
+    console.log('placeholders ::', placeholders);
+  }
+
+  name(): string {
+    return `${this.user}`;
+  }
+
+  getGroup(): string {
+    return this.group;
+  }
+
+  hi(strings: TemplateStringsArray, name: string): string {
+    return `${strings[0]} ${name.toUpperCase()}${strings[1]}`;
   }
 }
